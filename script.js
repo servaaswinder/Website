98e359bcd8645779c06991350211b2fef53c8c4b
Servaas <servaaswinder@gmail.com>
2023-09-19 20:44:14 +0200

    asd

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const links = document.querySelectorAll('.gallery a');
+const links = document.querySelectorAll(".gallery a");
 
-links.forEach(link => {
-  link.addEventListener('click', (e) => {
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
-    const category = link.getAttribute('href');
+    const category = link.getAttribute("href");
     const element = document.querySelector(category);
     element.scrollIntoView({ behavior: "smooth" });
   });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navbar = document.getElementById('navbar');
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navbar = document.getElementById("navbar");
 
-  menuToggle.addEventListener('click', function () {
-    navbar.classList.toggle('is-open');
+  menuToggle.addEventListener("click", function () {
+    navbar.classList.toggle("is-open");
   });
+});
