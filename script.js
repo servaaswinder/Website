98cd2cb33c8ac240d7093f71db46c3be3798bc5e
servaaswinder <servaaswinder@gmail.com>
2023-09-19 13:20:55 +0200

    asd

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ links.forEach(link => {
     e.preventDefault();
     const category = link.getAttribute('href');
     const element = document.querySelector(category);
-    element.scrollIntoView({behavior: "smooth"});
+    element.scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navbar = document.getElementById('navbar');
+
+  menuToggle.addEventListener('click', function () {
+    navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
   });
 });
